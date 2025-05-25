59d54708ccc70214ff1c74e0f2b0fe321885a8cf
BNM <ban.marcell@gmail.com>
2025-05-25 22:13:27 +0200

    Audio features - TTS - STT

---
+++ lib/Audio.py
@@ -0,0 +1,91 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import platform
+
+
+def play_audio_mac(file_path):
+    """Plays an audio file on macOS using osascript."""
+    try:
+        os.system(f"open '{file_path}'")  # Simplified osascript command
+    except Exception as e:
+        print(f"Error playing audio on macOS: {e}")
+
+def play_audio_linux(file_path):
+    """Plays an audio file on Linux using mpg321."""
+    try:
+        os.system(f"mpg321 '{file_path}'")
+    except Exception as e:
+        print(f"Error playing audio on Linux: {e}")
+
+def play_audio_windows(file_path):
+    """Plays an audio file on Windows using the default media player."""
+    try:
+        os.system(f"start '{file_path}'")  # Use start for Windows
+    except Exception as e:
+        print(f"Error playing audio on Windows: {e}")
+
+
+def play_audio_cross_platform(file_path):
+    """
+    Plays an audio file based on the operating system.
+    """
+    os_name = platform.system()
+    if os_name == "Darwin":
+        play_audio_mac(file_path)
+    elif os_name == "Linux":
+        play_audio_linux(file_path)
+    elif os_name == "Windows":
+        play_audio_windows(file_path)
+    else:
+        print(f"Unsupported operating system: {os_name}")
+
+
+def gtts_lang(language):
+    return language.split("-")[0]
+
+
+def speech_to_text(language):
+    """
+    Converts speech to text using Google's Speech-to-Text API.
+    """
+
+    text = ""
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+    with microphone as source:
+        print(f"Please speak in {language}:")
+        audio = recognizer.listen(source)
+    try:
+        text = recognizer.recognize_google(audio, language=language)
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return text
+
+
+def text_to_speech(text, language="en-US"):
+    """
+    Converts text to speech using Google's Text-to-Speech API.
+    """
+    # Text-to-Speech
+    tts = gTTS(text=text, lang=gtts_lang(language), slow=False)
+    tts.save("echoed_text.mp3")
+
+    play_audio_cross_platform("echoed_text.mp3")  # Use cross-platform playback
+
+
+def echo_speech(language="en-US"):
+    """
+    Listens for speech input, echoes it back via TTS.
+    """
+    text = speech_to_text(language)
+    if text:
+        text_to_speech(text, language)
+
+
+if __name__ == "__main__":
+    echo_speech(language="en-US")
+    #echo_speech(language="hu-HU")
