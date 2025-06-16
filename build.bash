98db30eaa32d5eb480d121bb7dd805b1b1a18cce
BNM <ban.marcell@gmail.com>
2025-06-16 21:30:12 +0200

    build.bash quick fix

--- build.bash
+++ build.bash
@@ -4,13 +4,13 @@ echo -e "Cleaning build artifacts..."
 rm -rf build/ dist/ *.egg-info
 
 echo -e "Building package..."
-python -m build || exit 1
+python3 -m build || exit 1
 
 echo -e "List packages..."
 ls -l dist
 
 echo -e "Reinstalling package..."
-pip install --force-reinstall --no-cache-dir dist/*.whl
+pip3 install --force-reinstall --no-cache-dir --break-system-packages dist/*.whl
 
 echo -e "New command:\n\tnolara\nDone. Test by running:"
 nolara
